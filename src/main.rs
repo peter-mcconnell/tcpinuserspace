782fce6319695e677c3f095794e6028f6fa3be30
Peter McConnell <me@petermcconnell.com>
2023-01-20 22:41:42 +0000

    tidying up ipv4 printing

--- src/main.rs
+++ src/main.rs
@@ -8,17 +8,20 @@ fn main() -> io::Result<()> {
     loop {
         let nbytes = nic.recv(&mut buf[..])?;
         // 3.2 in https://www.kernel.org/doc/Documentation/networking/tuntap.txt # tuntap
-        let flags = u16::from_be_bytes([buf[0], buf[1]]);
-        let proto = u16::from_be_bytes([buf[2], buf[3]]);
+        let eth_flags = u16::from_be_bytes([buf[0], buf[1]]);
+        let eth_proto = u16::from_be_bytes([buf[2], buf[3]]);
         // proto numbers in https://en.wikipedia.org/wiki/EtherType
-        if proto != 0x0800 {
+        if eth_proto != 0x0800 {
             // not ipv4
             continue;
         }
 
         match etherparse::Ipv4HeaderSlice::from_slice(&buf[4..nbytes]) {
             Ok(p) => {
-                eprintln!("read {} bytes: (flags: {:x}, proto: {:x}) {:?}", flags, proto, nbytes - 4, p);
+                let src = p.source_addr();
+                let dest = p.destination_addr();
+                let proto = p.protocol();
+                eprintln!("{} -> {} {}b of protocol {}", src, dest, proto, p.payload_len());
             },
             Err(e) => {
                 eprintln!("ignoring packet: {:?}", e);
