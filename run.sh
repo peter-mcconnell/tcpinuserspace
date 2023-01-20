040c77f5c9fb53552abe3e61513daeb6a3abf972
Peter McConnell <me@petermcconnell.com>
2023-01-20 22:28:54 +0000

    tidy up process on term

--- run.sh
+++ run.sh
@@ -9,6 +9,7 @@ cargo b --release
 sudo setcap CAP_NET_ADMIN=eip $CARGO_TARGET_DIR/release/tcpinuserspace
 $CARGO_TARGET_DIR/release/tcpinuserspace &
 pid=$!
+trap "kill -9 $!" TERM
 sudo ip addr add 192.168.0.1/24 dev tun0
 sudo ip link set dev tun0 up
 wait $pid
