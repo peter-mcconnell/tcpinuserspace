1d19eae12c55b9f7f5e15c50e27be7271b7c77e6
Peter McConnell <me@petermcconnell.com>
2023-01-20 22:37:16 +0000

    parse ipv4

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,20 @@ fn main() -> io::Result<()> {
         // 3.2 in https://www.kernel.org/doc/Documentation/networking/tuntap.txt # tuntap
         let flags = u16::from_be_bytes([buf[0], buf[1]]);
         let proto = u16::from_be_bytes([buf[2], buf[3]]);
-        eprintln!("read {} bytes: (flags: {:x}, proto: {:x}) {:x?}", flags, proto, nbytes - 4, &buf[4..nbytes]);
+        // proto numbers in https://en.wikipedia.org/wiki/EtherType
+        if proto != 0x0800 {
+            // not ipv4
+            continue;
+        }
+
+        match etherparse::Ipv4HeaderSlice::from_slice(&buf[4..nbytes]) {
+            Ok(p) => {
+                eprintln!("read {} bytes: (flags: {:x}, proto: {:x}) {:?}", flags, proto, nbytes - 4, p);
+            },
+            Err(e) => {
+                eprintln!("ignoring packet: {:?}", e);
+            }
+        }
     }
     Ok(())
 }
