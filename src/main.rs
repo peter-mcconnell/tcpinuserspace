99053721ca110759a9b858f2e8977297dec52b07
Peter McConnell <me@petermcconnell.com>
2023-01-22 12:14:56 +0000

    minor cleanups

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,6 @@ fn main() -> io::Result<()> {
     loop {
         let nbytes = nic.recv(&mut buf[..])?;
         // 3.2 in https://www.kernel.org/doc/Documentation/networking/tuntap.txt # tuntap
-        let eth_flags = u16::from_be_bytes([buf[0], buf[1]]);
         let eth_proto = u16::from_be_bytes([buf[2], buf[3]]);
         // proto numbers in https://en.wikipedia.org/wiki/EtherType
         if eth_proto != 0x0800 {
@@ -21,12 +20,11 @@ fn main() -> io::Result<()> {
                 let src = p.source_addr();
                 let dest = p.destination_addr();
                 let proto = p.protocol();
-                eprintln!("{} -> {} {}b of protocol {}", src, dest, proto, p.payload_len());
+                eprintln!("{} -> {} {}b of protocol {}", src, dest, p.payload_len(), proto);
             },
             Err(e) => {
                 eprintln!("ignoring packet: {:?}", e);
             }
         }
     }
-    Ok(())
 }
