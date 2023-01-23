caab5cfa65efb56c70222d8d39154403992c869d
Peter McConnell <me@petermcconnell.com>
2023-01-23 21:48:26 +0000

    starting work on tcp

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,15 @@
 use std::io;
+use std::collections::HashMap;
+use std::net::Ipv4Addr;
 extern crate tun_tap;
+
+mod tcp;
+
+struct Quad {
+    src: (Ipv4Addr, u16),
+    dest: (Ipv4Addr, u16),
+}
+
 
 fn main() -> io::Result<()> {
     let nic = tun_tap::Iface::new("tun0", tun_tap::Mode::Tun)?;
@@ -16,11 +26,32 @@ fn main() -> io::Result<()> {
         }
 
         match etherparse::Ipv4HeaderSlice::from_slice(&buf[4..nbytes]) {
-            Ok(p) => {
-                let src = p.source_addr();
-                let dest = p.destination_addr();
-                let proto = p.protocol();
-                eprintln!("{} -> {} {}b of protocol {}", src, dest, p.payload_len(), proto);
+            Ok(iph) => {
+                let src = iph.source_addr();
+                let dest = iph.destination_addr();
+                if iph.protocol() != 0x06 {
+                    // not tcp
+                    continue;
+                }
+
+                match etherparse::TcpHeaderSlice::from_slice(&buf[4 + iph.slice().len()..]) {
+                    Ok(tcph) => {
+                        let data = 4 + iph.slice().len() + tcph.slice().len();
+                        connections.entry(Quad {
+                            src: (src, tcph.source_port()),
+                            dst: (dest, tcph.destination_port()),
+                        }).or_default().on_packet(iph, tcph, &buf[p]);
+                        eprintln!(
+                            "{} -> {} {}b of tcp to port {}",
+                            src,
+                            dest,
+                            tcph.slice().len(),
+                            tcph.destination_port()
+                        );
+                    }
+                    Err(e) => {
+                        eprintln!("ignoring weird tcp packet {:?}", e);
+                    }
             },
             Err(e) => {
                 eprintln!("ignoring packet: {:?}", e);
